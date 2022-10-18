2f80d936fc9bec339d4099f5ccde30ca33df7ae0
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2022-10-18 23:58:49 +0800

    Fix: set Django to dev mode as default

--- inertia_django_vite_vue_minimal/settings.py
+++ inertia_django_vite_vue_minimal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0-s3nzb$^1yk(!-_e553a!&b(74^d1rg()vziok)h33!oz)mhv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
